8e1305486202c1290e2f34775c6b23d44d1afb38
maggie-hughes <32142470+maggie-hughes@users.noreply.github.com>
2017-11-05 12:52:23 -0500

    Update BarChart2.js

--- BarChart2.js
+++ BarChart2.js
@@ -1,14 +1,34 @@
 var svg = d3.select("body").select("#svg_BarChart2");
 var data = [
-  {id: "Time", date: "June 12", number of shares: 130},
-  {id: "Clashdaily", date: "June 12", number of shares: 119},
-  {id: "Youtube", date: "June 12", number of shares: 86},
-  {id: "PoliticusUSA", date: "June 12", number of shares: 76},
-  {id: "Time", date: "June 12", number of shares: 68},
-  {id: "OccupyDemocrats", date: "June 12", number of shares: 66},
-  {id: "USAToday", date: "June 12", number of shares: 63},
-  {id: "ZeroHedge", date: "June 12", number of shares: 56},
-  {id: "AmericanThinker", date: "June 12", number of shares: 55}
+  {id: "Time", date: "June 12", shares: 130},
+  {id: "Clashdaily", date: "June 12", shares: 119},
+  {id: "Youtube", date: "June 12", shares: 86},
+  {id: "PoliticusUSA", date: "June 12", shares: 76},
+  {id: "Time", date: "June 12", shares: 68},
+  {id: "OccupyDemocrats", date: "June 12", shares: 66},
+  {id: "USAToday", date: "June 12", shares: 63},
+  {id: "ZeroHedge", date: "June 12", shares: 56},
+  {id: "AmericanThinker", date: "June 12", shares: 55},
+  {id: "Youtube", date: "June 13", shares: 68},
+  {id: "NewYorkTimes", date: "June 13", shares: 59},
+  {id: "PJMedia", date: "June 13", shares: 57},
+  {id: "WashingtonPost", date: "June 13", shares: 53},
+  {id: "NewYorkTimes", date: "June 13", shares: 53},
+  {id: "Breitbart", date: "June 13", shares: 44},
+  {id: "People", date: "June 13", shares: 29},
+  {id: "TheAtlantic", date: "June 13", shares: 28},
+  {id: "Twitter", date: "June 13", shares: 25},
+  {id: "CNN", date: "June 13", shares: 25},
+  {id: "Youtube", date: "June 14", shares: 65},
+  {id: "FoxNews", date: "June 14", shares: 31},
+  {id: "TheHill", date: "June 14", shares: 24},
+  {id: "PJMedia", date: "June 14", shares: 24},
+  {id: "NewYorkTimes", date: "June 14", shares: 22},
+  {id: "TheAtlantic", date: "June 14", shares: 17},
+  {id: "HuffingtonPost", date: "June 14", shares: 17},
+  {id: "CBS", date: "June 14", shares: 16},
+  {id: "CNN", date: "June 14", shares: 16},
+  {id: "WashingtonPost", date: "June 14", shares: 13}
   
   
 ];
@@ -18,5 +38,5 @@ var visualization = d3plus.viz()
   .type("bar")
   .id("id")
   .x("date")
-  .y("number of shares")
+  .y("shares")
   .draw();
